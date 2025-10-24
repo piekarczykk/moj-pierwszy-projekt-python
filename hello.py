417e0c0969de38ad8afdb18b2c71046727593551
Krzysztof Piekarczyk <unikris94@gmail.com>
2025-10-24 15:55:54 +0200

    Dodalem f-string

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
 print("Hello, Git!")
+
+imie = "Krzysztof" # zmienna string
+wiek = 31 #zmienna int
+
+print("Tu ", imie)
+print("Aktualnie licze sobie ",wiek)
+print(f"f-string to dziala?, Czesc tu {imie}, mam {wiek} lat")
